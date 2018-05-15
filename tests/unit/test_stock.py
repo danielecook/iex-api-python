ec3b58102b12ee4eb18b26156197d5d47d87384b
Daniel Cook <danielecook@gmail.com>
2018-05-15 09:08:44 +0100

    update test

--- tests/unit/test_stock.py
+++ tests/unit/test_stock.py
@@ -1,11 +1,15 @@
-from datetime import date
-
 from tests import *
 from tests.helpers import *
 
 from iex import stock
 
+
 def test_stock_book():
     aapl = stock("AAPL")
-    assert aapl.book()['symbol'] == 'AAPL'
+    assert aapl.book()['quote']['symbol'] == 'AAPL'
 
+
+def test_stock_chart():
+    tsla = stock("TSLA")
+    tsla.chart(range='1d')
+    assert t.chart(range='20180514')[0]['date'] == '20180514'
