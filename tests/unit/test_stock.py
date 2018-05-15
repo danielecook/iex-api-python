16c0522973856bee77ed95ba7ead3f237b50b6cb
Daniel Cook <danielecook@gmail.com>
2018-05-15 09:18:36 +0100

    add error and other test

--- tests/unit/test_stock.py
+++ tests/unit/test_stock.py
@@ -1,3 +1,4 @@
+from pytest import raises
 from tests import *
 from tests.helpers import *
 
@@ -13,3 +14,15 @@ def test_stock_chart():
     tsla = stock("TSLA")
     tsla.chart(range='1d')
     assert tsla.chart(range='20180514')[0]['date'] == '20180514'
+
+
+def test_chart_table():
+    msft = stock("MSFT")
+    msft_chart = msft.chart_table()
+    assert msft_chart.empty == False
+
+
+def test_chart_range():
+    msft = stock("MSFT")
+    with pytest.raises(ValueError):
+        msft.chart(range="not_a_real_range")
