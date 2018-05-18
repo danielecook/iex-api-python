05e15f9b986dedf1edc4002a577570bf29825e0c
Daniel Cook <danielecook@gmail.com>
2018-05-18 11:34:39 +0100

    Improve dev flow

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 import setuptools
+import glob
+import os
+
+required = [
+    "requests",
+    "pandas",
+    "arrow",
+    "socketIO-client-nexus"
+]
 
 setuptools.setup(name='iex-api-python',
-                 version="0.0.2",
+                 version="0.0.3",
                  description='Fetch data from the IEX API',
                  long_description=open('README.md').read().strip(),
                  author='Daniel E. Cook',
                  author_email='danielecook@gmail.com',
                  url='http://www.github.com/danielecook/iex-api-python',
-                 py_modules=['iex'],
-                 install_requires=[],
+                 packages=['iex'],
+                 install_requires=required,
                  keywords=['finance', 'stock', 'market', 'market-data', 'IEX', 'API'],
                  license='MIT License',
                  zip_safe=False)
